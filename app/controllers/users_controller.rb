4963a94f703c6610c44c66f3364ee9c33d5f5d9f
Nabil <nabilalsouhily@gmail.com>
2016-12-22 18:38:40 +0800

    DoingGreat

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,25 @@ class UsersController < ApplicationController
 
 	def new 
 		@user = User.new
-		render :"index.html/new"
 	end
 
 	def create
-	@user = User.new
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to users_path
+
+		else
+			render 'new'
+		end
 	end
+
+		def edit
+			end 
+		def update
+			end 
+			private
+def user_params
+params.require(:user).permit(:first_name, :last_name, :email, :password)
+end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  include Clearance::User
+
 	
 has_many :listings, dependent: :destroy
 has_many :reservations, dependent: :destroy
